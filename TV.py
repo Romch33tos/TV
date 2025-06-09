64b602672a5dfb2f121e7756e0f3f72c94824050
Roman Khadraliev <Romch33tos@yandex.ru>
2025-06-09 18:30:55 +0700

    Замена формата файла кадров

--- TV.py
+++ TV.py
@@ -9,31 +9,31 @@ def start_animation():
 
 def show_frame_3():
   display_label.configure(image = frame_3)
-  root.after(500, show_frame_4)
+  root.after(450, show_frame_4)
 
 def show_frame_4():
   display_label.configure(image = frame_4)
-  root.after(500, show_frame_5)
+  root.after(450, show_frame_5)
 
 def show_frame_5():
   display_label.configure(image = frame_5)
-  root.after(500, show_frame_6)
+  root.after(450, show_frame_6)
 
 def show_frame_6():
   display_label.configure(image = frame_6)
-  root.after(500, show_frame_7)
+  root.after(450, show_frame_7)
 
 def show_frame_7():
   display_label.configure(image = frame_7)
-  root.after(500, show_frame_8)
+  root.after(450, show_frame_8)
 
 def show_frame_8():
   display_label.configure(image = frame_8)
-  root.after(500, show_frame_9)
+  root.after(450, show_frame_9)
 
 def show_frame_9():
   display_label.configure(image = frame_9)
-  root.after(500, show_frame_10)
+  root.after(450, show_frame_10)
 
 def show_frame_10():
   display_label.configure(image = frame_10)
@@ -49,16 +49,16 @@ root.configure(bg = "salmon4")
 root.geometry("400x280")
 root.resizable(width = False, height = False)
 
-frame_1 = ImageTk.PhotoImage(file = "frames/frame_1.jpg")
-frame_2 = ImageTk.PhotoImage(file = "frames/frame_2.jpg")
-frame_3 = ImageTk.PhotoImage(file = "frames/frame_3.jpg")
-frame_4 = ImageTk.PhotoImage(file = "frames/frame_4.jpg")
-frame_5 = ImageTk.PhotoImage(file = "frames/frame_5.jpg")
-frame_6 = ImageTk.PhotoImage(file = "frames/frame_6.jpg")
-frame_7 = ImageTk.PhotoImage(file = "frames/frame_7.jpg")
-frame_8 = ImageTk.PhotoImage(file = "frames/frame_8.jpg")
-frame_9 = ImageTk.PhotoImage(file = "frames/frame_9.jpg")
-frame_10 = ImageTk.PhotoImage(file = "frames/frame_10.jpg")
+frame_1 = ImageTk.PhotoImage(file = "frames/frame_1.png")
+frame_2 = ImageTk.PhotoImage(file = "frames/frame_2.png")
+frame_3 = ImageTk.PhotoImage(file = "frames/frame_3.png")
+frame_4 = ImageTk.PhotoImage(file = "frames/frame_4.png")
+frame_5 = ImageTk.PhotoImage(file = "frames/frame_5.png")
+frame_6 = ImageTk.PhotoImage(file = "frames/frame_6.png")
+frame_7 = ImageTk.PhotoImage(file = "frames/frame_7.png")
+frame_8 = ImageTk.PhotoImage(file = "frames/frame_8.png")
+frame_9 = ImageTk.PhotoImage(file = "frames/frame_9.png")
+frame_10 = ImageTk.PhotoImage(file = "frames/frame_10.png")
 
 display_label = Label(root, image = frame_1)
 display_label.grid(row = 0, column = 0, padx = 15, pady = 10, sticky = "nw")
